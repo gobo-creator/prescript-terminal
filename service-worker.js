807b73b51cded2b0581fff71468bf6c3ee430ed2
gobo-creator <gobogobo158@gmail.com>
2026-09-15 04:24:09 +0900

    Refresh cache for compact three-line iPhone codes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "prescript-terminal-v14-2-force-center";
+const CACHE_NAME = "prescript-terminal-v14-3-three-lines";
 const APP_SHELL = ["./","./index.html","./iphone.html","./style_iphone_fixed.css","./iphone.css","./script.js","./manifest.json","./iphone-manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith("prescript-terminal-")&&key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim();});
